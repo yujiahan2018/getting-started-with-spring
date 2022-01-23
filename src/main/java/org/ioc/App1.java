2e7f51fa33240c162c561c3ebec679dc6dd2194d
于家汉 <yujiahan_biancheng@163.com>
2022-01-23 16:52:21 +0800

    spring框架 ioc探索

--- src/main/java/org/ioc/App1.java
+++ src/main/java/org/ioc/App1.java
@@ -1,108 +1,41 @@
 package org.ioc;
 
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+import org.springframework.core.io.ClassPathResource;
+import java.util.Iterator;
+import java.util.List;
 
-
-public class App1 {
-    public static void main(String[] args) {
-        ApplicationContext ap1 =
-                new ClassPathXmlApplicationContext("applicationContext.xml");
-
-
-        System.out.println("----------------独立演示----------------");
-        System.out.println("--------构造方法注入示例--------");
-        MockBusinessObject1 ml1 = (MockBusinessObject1) ap1.getBean("mockBO1");
-        System.out.println("type属性: " + ml1);
-        MockBusinessObject2 ml2 = (MockBusinessObject2) ap1.getBean("mockBO2");
-        System.out.println("index属性: " + ml2);
-
-        System.out.println("--------set方法注入示例--------");
-        MockBusinessObject3 ml3 = (MockBusinessObject3) ap1.getBean("mockBO3");
-        System.out.println(ml3);
-
-
-        System.out.println("\n----------------合并演示----------------");
-        MockBusinessObject4 ml4 = (MockBusinessObject4) ap1.getBean("mockBO4");
-        System.out.println(ml4);
-
+class ClassPathResourceTest
+{
+    public static void main(String[] args)
+            throws Exception
+    {
+        // 创建一个Resource对象，从类加载路径里读取资源
+        ClassPathResource cr = new ClassPathResource("applicationContext.xml");
+        // 获取该资源的简单信息
+        System.out.println(cr.getFilename());
+        System.out.println(cr.getDescription());
+        // 创建基于SAX的dom4j解析器
+        SAXReader reader = new SAXReader();
+        Document doc = reader.read(cr.getFile());
+        // 获取根元素
+        Element el = doc.getRootElement();
+        List l = el.elements();
+        // 遍历根元素的全部子元素
+        for (Iterator it = l.iterator();it.hasNext() ; )
+        {
+            // 每个节点都是<书>节点
+            Element book = (Element)it.next();
+            List ll = book.elements();
+            // 遍历<书>节点的全部子节点
+            for (Iterator it2 = ll.iterator();it2.hasNext() ; )
+            {
+                Element eee = (Element)it2.next();
+                System.out.println(eee.getText());
+            }
+        }
     }
 }
 
-
-class MockBusinessObject4 {
-    private final String dependency4_1;
-    private String dependency4_2;
-
-    public MockBusinessObject4(String dependency) {
-        this.dependency4_1 = dependency;
-    }
-
-    public void setDependency4(String dependency4) {
-        this.dependency4_2 = dependency4;
-    }
-
-    @Override
-    public String toString() {
-        return "MockBusinessObject4{" +
-                "dependency4_1='" + dependency4_1 + '\'' +
-                ", dependency4_2='" + dependency4_2 + '\'' +
-                '}';
-    }
-}
-
-class MockBusinessObject3 {
-    private String dependency3;
-
-    public void setDependency3(String dependency3) {
-        this.dependency3 = dependency3;
-    }
-
-    @Override
-    public String toString() {
-        return "MockBusinessObject3{" +
-                "dependency3='" + dependency3 + '\'' +
-                '}';
-    }
-}
-
-class MockBusinessObject2 {
-    private final String dependency1;
-    private final String dependency2;
-
-    public MockBusinessObject2(String dependency1, String dependency2) {
-        this.dependency1 = dependency1;
-        this.dependency2 = dependency2;
-    }
-
-
-    @Override
-    public String toString() {
-        return new ToStringBuilder(this)
-                .append("dependency1", dependency1)
-                .append("dependency2", dependency2).toString();
-    }
-}
-
-
-class MockBusinessObject1 {
-    private String dependency1;
-    private int dependency2;
-
-
-    public MockBusinessObject1(String dependency) {
-        this.dependency1 = dependency;
-    }
-
-    public MockBusinessObject1(int dependency) {
-        this.dependency2 = dependency;
-    }
-
-
-    public String toString() {
-        return new ToStringBuilder(this)
-                .append("dependency1", dependency1)
-                .append("dependency2", dependency2).toString();
-    }
-}
